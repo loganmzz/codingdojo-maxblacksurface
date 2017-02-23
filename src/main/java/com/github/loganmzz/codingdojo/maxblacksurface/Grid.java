50d30246cb8a147f2794d5a1de225b45c98c5ce2
Logan Mzz <loganmzz@users.noreply.github.com>
2017-02-23 23:51:44 +0100

    Check input: 01 03

--- src/main/java/com/github/loganmzz/codingdojo/maxblacksurface/Grid.java
+++ src/main/java/com/github/loganmzz/codingdojo/maxblacksurface/Grid.java
@@ -1,12 +1,32 @@
 package com.github.loganmzz.codingdojo.maxblacksurface;
 
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Grid {
 
 
     private int maxBlackSurface;
 
-    public Grid(String s) {
-        maxBlackSurface = s.isEmpty() ? 0 : s.split(",").length;
+    public Grid(String blackCellCoordinates) {
+        if (!blackCellCoordinates.isEmpty()) {
+            int[][] grid = new int[10][10];
+            for (String blackCellCoordinate : blackCellCoordinates.split(",")) {
+                int colIndex = Integer.parseInt(blackCellCoordinate.substring(0, 1));
+                int rowIndex = Integer.parseInt(blackCellCoordinate.substring(1, 2));
+                grid[colIndex][rowIndex] = 1;
+            }
+
+            for (int colIndex = 0; colIndex < grid.length; colIndex++) {
+                for (int rowIndex = 0; rowIndex < grid[colIndex].length; rowIndex++) {
+                    if (grid[colIndex][rowIndex] == 1) {
+                        if (colIndex > 0) grid[colIndex][rowIndex] += grid[colIndex-1][rowIndex  ];
+                        if (rowIndex > 0) grid[colIndex][rowIndex] += grid[colIndex  ][rowIndex-1];
+                    }
+                }
+            }
+            maxBlackSurface = Stream.of(grid).flatMapToInt(col -> IntStream.of(col)).max().getAsInt();
+        }
     }
 
     public int getMaxBlackSurface() {
